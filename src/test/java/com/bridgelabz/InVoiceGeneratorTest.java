5e93b7f050f4fe5d06e81d84813bd55a4de8e8e1
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-03-21 21:51:35 +0530

    Initial commit

---
+++ src/test/java/com/bridgelabz/InVoiceGeneratorTest.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class InVoiceGeneratorTest {
+    InVoiceGenerator InVoiceGenerator;
+
+    @BeforeEach
+    public void setUp(){
+      InVoiceGenerator inVoiceGenerator  = new InVoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        InVoiceGenerator inVoiceGenerator = new InVoiceGenerator();
+        double fare = inVoiceGenerator.calculateFare(distance, time);
+        double diff = Math.abs(25 - fare);
+        Assert.assertEquals(25.0, fare, diff);
+    }
+
+    @Test
+    public void givenLessDistanceOfTime_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        InVoiceGenerator invoiceGenerator = new InVoiceGenerator();
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        double diff = Math.abs(5 - fare);
+        Assert.assertEquals(5,fare, diff);
+    }
+
+}
