e3d11a337aa464133d2c87932a58a39988388890
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-03-22 17:13:39 +0530

    Added code to get UserID

---
+++ src/main/java/com/bridgelabz/RideRepository.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RideRepository{
+    private static HashMap<Integer, ArrayList<InvoiceSummary>> userData = new HashMap<>();
+    public static ArrayList<InvoiceSummary> getUserRideList(int userID) {
+        return userData.get(userID);
+    }
+    public static void addRide(int userId, InvoiceSummary summary){
+        ArrayList<InvoiceSummary> userInvoice = userData.get(userId);
+        if(userInvoice == null){
+            userInvoice = new ArrayList<>();
+        }
+        userInvoice.add(summary);
+        userData.put(userId,userInvoice);
+    }
+
+
+
+}
